93d68403fb76957b97e3575f49f07fbce3518941
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2021-08-03 17:05:18 +0200

    Extend stellna to a 2nd-order near-axis expansion.
    The class `equilibrium_stellna` is now being provided with its members
    Covariant, Contravariant, Magnitude, and Jacobian overriden, along with
    their respective derivatives. In this way, the need to explicity call
    the metric components (which are correct only to 2nd-order terms in the
    inverse aspect ratio) is avoided for lowering indices or other common
    tasks. An error is issued if metric components are explicitly asked for.
    The class `parser_stellna` was also deleted.

--- include/gyronimo/fields/equilibrium_stellna.hh
+++ include/gyronimo/fields/equilibrium_stellna.hh
@@ -15,31 +15,27 @@ namespace gyronimo{
 //! Quasi-symmetric stellarator equilibrium field in near-axis coordinates.
 /*!
     Following IR3Field rules, the magnetic field is normalised by a `m_factor`
-    matching its on-axis value `axis_field()` in [T]. In turn, 'axis_length()'
+    matching its reference value `Bref` in [T]. In turn, 'axis_length()'
     is in [m]. The coordinates are set by the `metric_stellna` object and the
     contravariant field components have dimensions of [m^{-1}]. Being an
     **equilibrium** field, `t_factor` is set to one.
 */
 class equilibrium_stellna : public IR3field_c1{
  public:
-  equilibrium_stellna(
-      const metric_stellna *g,
-      double axis_field, double axis_length, double axis_iota_minus_n);
+  equilibrium_stellna(const metric_stellna *g);
   virtual ~equilibrium_stellna() override {};
 
   virtual IR3 contravariant(const IR3& position, double time) const override;
-  virtual dIR3 del_contravariant(
-      const IR3& position, double time) const override;
-  virtual IR3 partial_t_contravariant(
-      const IR3& position, double time) const override {return {0.0,0.0,0.0};};
+  virtual dIR3 del_contravariant(const IR3& position, double time) const override;
+  virtual IR3 partial_t_contravariant(const IR3& position, double time) const override {return {0.0,0.0,0.0};};
+  virtual IR3 covariant(const IR3& position, double time) const override;
+  virtual dIR3 del_covariant(const IR3& position, double time) const override;
+  virtual IR3 partial_t_covariant(const IR3& position, double time) const override {return {0.0,0.0,0.0};};
+  virtual double magnitude(const IR3& position, double time) const override;
+  virtual IR3 del_magnitude(const IR3& position, double time) const override;
 
-  const metric_stellna* metric() const {return metric_;};
-  double axis_field() const {return this->m_factor();};
-  double axis_length() const {return axis_length_;};
-
- private:
-  const metric_stellna *metric_;
-  double axis_length_, length_factor_, iota_factor_;
+  private:
+    const metric_stellna *metric_;
 };
 
 }// end namespace gyronimo.
--- src/metric_stellna.cc
+++ src/metric_stellna.cc
@@ -4,161 +4,100 @@
 
 // @metric_stellna.cc
 
+#include <math.h>
 #include <numbers>
 #include <gyronimo/metrics/metric_stellna.hh>
+#include <gyronimo/core/error.hh>
 
 namespace gyronimo{
 
-metric_stellna::metric_stellna(
-    const parser_stellna* parser, const interpolator1d_factory* ifactory)
-    : sigma_(nullptr), curvature_(nullptr), torsion_(nullptr), dldphi_(nullptr),
-      phi_modulus_factor_(2*std::numbers::pi/parser->field_periods()),
-      eta_bar_(parser->eta_bar()) {
-  dblock_adapter phi_grid(parser->phi_grid());
-  sigma_ = ifactory->interpolate_data(
-      phi_grid, dblock_adapter(parser->sigma()));
-  dldphi_ = ifactory->interpolate_data(
-      phi_grid, dblock_adapter(parser->dldphi()));
-  torsion_ = ifactory->interpolate_data(
-      phi_grid, dblock_adapter(parser->torsion()));
-  curvature_ = ifactory->interpolate_data(
-      phi_grid, dblock_adapter(parser->curvature()));
-}
-metric_stellna::metric_stellna(
-    int field_periods, double eta_bar,
-    const dblock& phi_grid, const dblock& sigma,
-    const dblock& dldphi, const dblock& torsion, const dblock& curvature,
+metric_stellna::metric_stellna(int field_periods, double Bref, const dblock& phi_grid,
+    double G0, double G2, double I2, double iota, double iotaN,
+    const dblock& B0,    const dblock& B1c,    const dblock& B1s, 
+    const dblock& B20,   const dblock& B2c,    const dblock& B2s, 
+    const dblock& beta0, const dblock& beta1c, const dblock& beta1s, 
     const interpolator1d_factory* ifactory)
-    : sigma_(nullptr), curvature_(nullptr), torsion_(nullptr), dldphi_(nullptr),
-      phi_modulus_factor_(2*std::numbers::pi/field_periods),
-      eta_bar_(eta_bar) {
-  sigma_ = ifactory->interpolate_data(phi_grid, sigma);
-  dldphi_ = ifactory->interpolate_data(phi_grid, dldphi);
-  torsion_ = ifactory->interpolate_data(phi_grid, torsion);
-  curvature_ = ifactory->interpolate_data(phi_grid, curvature);
+    : phi_modulus_factor_(2*std::numbers::pi/field_periods), B0_(nullptr),
+    B1c_(nullptr), B1s_(nullptr), B20_(nullptr), B2c_(nullptr), B2s_(nullptr),
+    beta0_(nullptr), beta1c_(nullptr), beta1s_(nullptr){
+  Bref_   = Bref;
+  B0_     = ifactory->interpolate_data(phi_grid, B0);
+  B1c_    = ifactory->interpolate_data(phi_grid, B1c);
+  B1s_    = ifactory->interpolate_data(phi_grid, B1s);
+  B20_    = ifactory->interpolate_data(phi_grid, B20);
+  B2c_    = ifactory->interpolate_data(phi_grid, B2c);
+  B2s_    = ifactory->interpolate_data(phi_grid, B2s);
+  beta0_  = ifactory->interpolate_data(phi_grid, beta0);
+  beta1c_ = ifactory->interpolate_data(phi_grid, beta1c);
+  beta1s_ = ifactory->interpolate_data(phi_grid, beta1s);
+  G0_     = G0;
+  G2_     = G2;
+  I2_     = I2;
+  iota_   = iota;
+  iotaN_  = iotaN;
+  field_periods_ = field_periods;
 }
+
 metric_stellna::~metric_stellna() {
-  if(curvature_) delete curvature_;
-  if(torsion_) delete torsion_;
-  if(dldphi_) delete dldphi_;
-  if(sigma_) delete sigma_;
+  if(B0_) delete B0_;
+  if(B1c_) delete B1c_;
+  if(B1s_) delete B1s_;
+  if(B20_) delete B20_;
+  if(B2c_) delete B2c_;
+  if(B2s_) delete B2s_;
+  if(beta0_) delete beta0_;
+  if(beta1c_) delete beta1c_;
+  if(beta1s_) delete beta1s_;
 }
+
 double metric_stellna::reduce_phi(double phi) const {
-  return std::fmod(phi, phi_modulus_factor_);
+  phi = std::fmod(phi, phi_modulus_factor_);
+  return (phi < 0 ? phi + phi_modulus_factor_ : phi);
 }
+
 SM3 metric_stellna::operator()(const IR3& position) const {
-  double phi = this->reduce_phi(position[IR3::w]);
-  double r = position[IR3::u], theta = position[IR3::v];
-  double coso = std::cos(theta), sino = std::sin(theta);
-  double l_prime = (*dldphi_)(phi);
-  double k = (*curvature_)(phi), k_prime = (*curvature_).derivative(phi);
-  double sigma = (*sigma_)(phi), sigma_prime = (*sigma_).derivative(phi);
-  double k_prime_over_k = k_prime/k;
-  double eta_over_k = eta_bar_/k;
-  double eta_over_k_squared = eta_over_k*eta_over_k;
-  double guu = std::pow(eta_over_k*coso, 2) +
-          std::pow((sino + sigma*coso)/eta_over_k, 2);
-  double guv = -r*eta_over_k_squared*sino*coso +
-      r/eta_over_k_squared*(sino + sigma*coso)*(coso - sigma*sino);
-  double guw = r*(sigma_prime*coso*(sino + sigma*coso) +
-      k_prime_over_k*(
-          std::pow(sino + sigma*coso, 2) -
-          std::pow(eta_over_k_squared*coso, 2)))/eta_over_k_squared;
-  double gvv = r*r*(std::pow(eta_over_k*sino, 2) +
-          std::pow((coso - sigma*sino)/eta_over_k, 2));
-  double factor_sigma_k = sigma*k_prime_over_k + 0.5*sigma_prime;
-  double gvw = r*r*(l_prime*(*torsion_)(phi) + (
-          0.5*sigma_prime +
-          factor_sigma_k*(coso*coso - sino*sino) +
-          coso*sino*(k_prime_over_k*(1.0 - sigma*sigma) - sigma*sigma_prime)
-      )/eta_over_k_squared + coso*sino*eta_over_k_squared*k_prime_over_k);
-  double gww = l_prime*l_prime*(1.0 - 2.0*eta_bar_*r*coso);
-  return {guu, guv, guw, gvv, gvw, gww};
+  error(__func__, __FILE__, __LINE__, "Explicit metric not available.", 1);
+  return {0, 0, 0, 0, 0, 0};
 }
 dSM3 metric_stellna::del(const IR3& position) const {
-  double phi = this->reduce_phi(position[IR3::w]);
-  double r = position[IR3::u], theta = position[IR3::v];
-  double coso = std::cos(theta), sino = std::sin(theta);
-  double l_prime = (*dldphi_)(phi), l_prime_prime = dldphi_->derivative(phi);
-  double torsion = (*torsion_)(phi), torsion_prime = torsion_->derivative(phi);
-  double k = (*curvature_)(phi), k_prime = (*curvature_).derivative(phi),
-      k_prime_prime = (*curvature_).derivative2(phi);
-  double sigma = (*sigma_)(phi), sigma_prime = (*sigma_).derivative(phi),
-      sigma_prime_prime = (*sigma_).derivative2(phi);
-  double eta_over_k = eta_bar_/k;
-  double eta_over_k_squared = eta_over_k*eta_over_k;
-  double eta_over_k_quad = eta_over_k_squared*eta_over_k_squared;
-  double k_prime_over_k = k_prime/k;
-  double factor_a = 1.0 + eta_over_k_quad - sigma*sigma;
-  double factor_b = sigma*sigma_prime - factor_a*k_prime_over_k;
-  double cos2o = coso*coso - sino*sino;
-  double d_u_guu = 0.0;
-  double d_v_guu = 2.0*(sino + sigma*coso)*(
-      coso - sigma*sino)/eta_over_k_squared - 2.0*eta_over_k_squared*coso*sino;
-  double d_w_guu =(
-      2.0*sino*sino*k_prime_over_k +
-      coso*sino*(4.0*sigma*k_prime_over_k + 2.0*sigma_prime) +
-      coso*coso*2.0*(sigma*sigma_prime - k_prime_over_k*(
-          eta_over_k_quad - sigma*sigma)))/eta_over_k_squared;
-  double d_u_guv = -coso*sino*eta_over_k_squared +
-      (sino + sigma*coso)*(coso - sigma*sino)/eta_over_k_squared;
-  double d_v_guv = -r*(cos2o*(sigma*sigma + eta_over_k_quad - 1.0) +
-          4.0*coso*sino*sigma)/eta_over_k_squared;
-  double d_w_guv = r*(
-      cos2o*(2.0*sigma*k_prime_over_k + sigma_prime) -
-      2.0*coso*sino*factor_b)/eta_over_k_squared;
-  double d_u_guw = (sino*sino*k_prime_over_k +
-      coso*sino*(2.0*sigma*k_prime_over_k + sigma_prime) +
-      coso*coso*(sigma*sigma_prime +
-          k_prime_over_k*(sigma*sigma - eta_over_k_quad)))/eta_over_k_squared;
-  double d_v_guw = r*(cos2o*(2.0*sigma*k_prime_over_k + sigma_prime) -
-      2.0*coso*sino*factor_b)/eta_over_k_squared;
-  double factor_k_prime2 = k_prime_over_k*k_prime_over_k + k_prime_prime/k;
-  double d_w_guw = r*(
-      sino*sino*factor_k_prime2 +
-      coso*sino*(2.0*sigma*factor_k_prime2 + 4.0*k_prime*sigma_prime/k +
-      sigma_prime_prime) + coso*coso*(
-          k_prime_over_k*k_prime_over_k*(3.0*eta_over_k_quad + sigma*sigma) +
-          4.0*sigma*sigma_prime*k_prime_over_k + sigma_prime*sigma_prime +
-          sigma*sigma_prime_prime + k_prime_prime/k*(
-              sigma*sigma - eta_over_k_quad)))/eta_over_k_squared;
-  double d_u_gvv = 2.0*r*(coso*coso - 2.0*coso*sino*sigma +
-      sino*sino*(eta_over_k_quad + sigma*sigma))/eta_over_k_squared;
-  double d_v_gvv = 2.0*r*r*(
-      coso*sino*(sigma*sigma + eta_over_k_quad - 1.0) -
-      sigma*cos2o)/eta_over_k_squared;
-  double d_w_gvv = 2.0*r*r*(coso*coso*k_prime_over_k -
-      coso*sino*(2.0*sigma*k_prime_over_k + sigma_prime) +
-      sino*sino*(sigma*sigma_prime -
-          (eta_over_k_quad - sigma*sigma)*k_prime_over_k))/eta_over_k_squared;
-  double d_u_gvw = 2.0*r*l_prime*torsion + r*(
-      sigma_prime + cos2o*(2.0*sigma*k_prime_over_k + sigma_prime) -
-      2.0*coso*sino*factor_b)/eta_over_k_squared;
-  double d_v_gvw = -r*r*(cos2o*factor_b +
-          2.0*coso*sino*(
-              2.0*sigma*k_prime_over_k + sigma_prime))/eta_over_k_squared;
-  double d_w_gvw = r*r*(eta_over_k_squared*(
-          l_prime*torsion_prime + torsion*l_prime_prime) +
-      0.5*sigma_prime_prime + sigma_prime*k_prime_over_k +
-      cos2o*(
-          sigma*(k_prime_over_k*k_prime_over_k + k_prime_prime/k) +
-          2.0*sigma_prime*k_prime_over_k + 0.5*sigma_prime_prime) -
-      coso*sino*(
-          k_prime_over_k*k_prime_over_k*(
-              sigma*sigma + 3.0*eta_over_k_quad - 1.0) +
-          4.0*sigma*sigma_prime*k_prime_over_k + sigma_prime*sigma_prime +
-          sigma*sigma_prime_prime - k_prime_prime/k*factor_a)
-      )/eta_over_k_squared;
-  double d_u_gww = -2.0*eta_bar_*l_prime*l_prime*coso;
-  double d_v_gww = l_prime*l_prime*2.0*eta_bar_*r*sino;
-  double d_w_gww = l_prime*l_prime_prime*(2.0 - 4.0*eta_bar_*r*coso);
+  error(__func__, __FILE__, __LINE__, "Explicit metric not available.", 1);
   return {
-      d_u_guu, d_v_guu, d_w_guu,
-      d_u_guv, d_v_guv, d_w_guv,
-      d_u_guw, d_v_guw, d_w_guw,
-      d_u_gvv, d_v_gvv, d_w_gvv,
-      d_u_gvw, d_v_gvw, d_w_gvw,
-      d_u_gww, d_v_gww, d_w_gww};
+      0, 0, 0,
+      0, 0, 0,
+      0, 0, 0,
+      0, 0, 0,
+      0, 0, 0,
+      0, 0, 0};
+}
+
+double metric_stellna::jacobian(const IR3& position) const {
+  double r     = position[IR3::u], theta = position[IR3::v];
+  double coso  = std::cos(theta),   sino = std::sin(theta);
+  double cos2o = std::cos(2*theta), sin2o = std::sin(2*theta);
+  double phi   = this->reduce_phi(position[IR3::w]);
+  double magB  = (*B0_)(phi)+r*((*B1c_)(phi)*coso+(*B1s_)(phi)*sino)+r*r*((*B20_)(phi)+(*B2c_)(phi)*cos2o+(*B2s_)(phi)*sin2o);
+  double G     = G0_+r*r*G2_;
+  double I     = r*r*I2_;
+  double jac   = r*Bref_*(G+iota_*I)/(magB*magB);
+  return jac;
+}
+
+IR3 metric_stellna::del_jacobian(const IR3& position) const {
+  double r        = position[IR3::u], theta = position[IR3::v];
+  double coso     = std::cos(theta),   sino = std::sin(theta);
+  double cos2o    = std::cos(2*theta), sin2o = std::sin(2*theta);
+  double phi      = this->reduce_phi(position[IR3::w]);
+  double magB     = (*B0_)(phi)+r*( (*B1c_)(phi)*coso+(*B1s_)(phi)*sino)+r*r*((*B20_)(phi)+(*B2c_)(phi)*cos2o+ (*B2s_)(phi)*sin2o);
+  double d_u_magB =              (  (*B1c_)(phi)*coso+(*B1s_)(phi)*sino)+2*r*((*B20_)(phi)+(*B2c_)(phi)*cos2o+ (*B2s_)(phi)*sin2o);
+  double d_v_magB =            r*( -(*B1c_)(phi)*sino+(*B1s_)(phi)*coso)+r*r*(          -2*(*B2c_)(phi)*sin2o+2*(*B2s_)(phi)*cos2o);
+  double d_w_magB = (*B0_).derivative(phi)+r*((*B1c_).derivative(phi)*coso+(*B1s_).derivative(phi)*sino)+r*r*((*B20_).derivative(phi)+(*B2c_).derivative(phi)*cos2o+(*B2s_).derivative(phi)*sin2o);
+  double G        = G0_+r*r*G2_;
+  double I        = r*r*I2_;
+  double jac      = r*Bref_*(G+iota_*I)/(magB*magB);
+  double d_u_jac  = Bref_*(G+iota_*I)/(magB*magB)+r*Bref_*(2*r*(G2_+iota_*I2_))/(magB*magB)-2*d_u_magB*jac/magB;
+  double d_v_jac  = -2*d_v_magB*jac/magB;
+  double d_w_jac  = -2*d_w_magB*jac/magB;
+  return {d_u_jac,d_v_jac,d_w_jac};
 }
 
 } // end namespace gyronimo
