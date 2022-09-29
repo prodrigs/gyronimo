c8c3e2dcddcb240a956ba9bdd3ffbe6815c29120
tikoh-station <mannyassuncao@hotmail.com>
2022-09-29 23:40:18 +0100

    Apply suggested changes to morphism classes

--- gyronimo/metrics/morphism_helena.hh
+++ gyronimo/metrics/morphism_helena.hh
@@ -1,6 +1,6 @@
 // ::gyronimo:: - gyromotion for the people, by the people -
 // An object-oriented library for gyromotion applications in plasma physics.
-// Copyright (C) 2022 Paulo Rodrigues.
+// Copyright (C) 2022 Paulo Rodrigues and Manuel Assunção.
 
 // ::gyronimo:: is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -20,11 +20,6 @@
 #ifndef GYRONIMO_MORPHISM_HELENA
 #define GYRONIMO_MORPHISM_HELENA
 
-#include <gsl/gsl_vector.h>
-#include <gsl/gsl_multiroots.h>
-#include <numbers>
-#include <gyronimo/core/error.hh>
-#include <gyronimo/core/multiroot.hh>
 #include <gyronimo/metrics/morphism.hh>
 #include <gyronimo/parsers/parser_helena.hh>
 #include <gyronimo/interpolators/interpolator2d.hh>
@@ -41,7 +36,7 @@ public:
 	virtual IR3 operator()(const IR3& q) const override;
 	virtual IR3 inverse(const IR3& x) const override;
 	virtual dIR3 del(const IR3& q) const override;
-	virtual ddIR3 del2(const IR3& q) const override;
+	virtual ddIR3 ddel(const IR3& q) const override;
 
 	virtual double jacobian(const IR3 &q) const override;
 	const parser_helena* parser() const {return parser_;};
--- gyronimo/metrics/morphism_spherical.cc
+++ gyronimo/metrics/morphism_spherical.cc
@@ -1,6 +1,6 @@
 // ::gyronimo:: - gyromotion for the people, by the people -
 // An object-oriented library for gyromotion applications in plasma physics.
-// Copyright (C) 2022 Paulo Rodrigues.
+// Copyright (C) 2022 Manuel Assunção.
 
 // ::gyronimo:: is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
 
 // @morphism_spherical.cc, this file is part of ::gyronimo::
 
+#include <cmath>
 #include <gyronimo/metrics/morphism_spherical.hh>
 
 namespace gyronimo {
@@ -27,19 +28,21 @@ namespace gyronimo {
 	Implements the coordinate transformation:
 	@f{gather*}{
 		\begin{aligned}
-			x &= r \, \cos \phi \, \sin \theta \\
-			y &= r \, \sin \phi \, \sin \theta \\
-			z &= r \, \cos \theta
+			x &= r_0 \, r \, \cos \phi \, \sin \theta \\
+			y &= r_0 \, r \, \sin \phi \, \sin \theta \\
+			z &= r_0 \, r \, \cos \theta
 		\end{aligned}
     @f}
 */
 IR3 morphism_spherical::operator()(const IR3 &q) const {
-	double r = q[IR3::u];
+	double r = r0_ * q[IR3::u];
 	double cn_theta = std::cos(q[IR3::v]);
 	double sn_theta = std::sin(q[IR3::v]);
 	double cn_phi = std::cos(q[IR3::w]);
 	double sn_phi = std::sin(q[IR3::w]);
-	return {r * cn_phi * sn_theta, r * sn_phi * sn_theta, r * cn_theta};
+	return {r * cn_phi * sn_theta, 
+			r * sn_phi * sn_theta, 
+			r * cn_theta};
 }
 
 //! Inverse transform from cartesian coordinates @f$ \textbf{x} @f$ into spherical coordinates @f$ q^\alpha @f$.
@@ -47,7 +50,7 @@ IR3 morphism_spherical::operator()(const IR3 &q) const {
 	Implements the inverse transformation:
 	@f{gather*}{
 		\begin{aligned}
-			r &= \sqrt{x^2 + y^2 + z^2} \\
+			r &= \frac{1}{r_0} \sqrt{x^2 + y^2 + z^2} \\
 			\theta &= \arctan \left( \frac{\sqrt{x^2 + y^2}}{z} \right) \\
 			\phi &= \arctan \left( \frac{y}{x} \right)
 		\end{aligned}
@@ -55,7 +58,9 @@ IR3 morphism_spherical::operator()(const IR3 &q) const {
 */
 IR3 morphism_spherical::inverse(const IR3 &x) const {
 	double rho = x[IR3::u] * x[IR3::u] + x[IR3::v] * x[IR3::v];
-	return {std::sqrt(rho + x[IR3::w] * x[IR3::w]), std::atan2(sqrt(rho), x[IR3::w]), std::atan2(x[IR3::v], x[IR3::u])};
+	return {ir0_ * std::sqrt(rho + x[IR3::w] * x[IR3::w]), 
+			std::atan2(sqrt(rho), x[IR3::w]), 
+			std::atan2(x[IR3::v], x[IR3::u])};
 }
 
 //! Returns the morphism's first derivatives, correspondent to the covariant basis vectors in point @f$ q^\alpha @f$.
@@ -63,47 +68,47 @@ IR3 morphism_spherical::inverse(const IR3 &x) const {
 	Implements the coordinate transformation's first derivatives:
 	@f{gather*}{
 		\begin{aligned}
-			\textbf{e}_r &= \left( \cos \phi \, \sin \theta, \sin \phi \, \sin \theta, \cos \theta \right) \\
-			\textbf{e}_\theta &= \left( r \, \cos \phi \, \cos \theta, r \, \sin \phi \, \cos \theta, -r \, \sin \theta \right) \\
-			\textbf{e}_\phi &= \left( -r \, \sin \phi \, \sin \theta, r\, \cos \phi \, \sin \theta, 0 \right)
+			\textbf{e}_r &= \left( r_0 \, \cos \phi \, \sin \theta, r_0 \, \sin \phi \, \sin \theta, r_0 \, \cos \theta \right) \\
+			\textbf{e}_\theta &= \left( r_0 \, r \, \cos \phi \, \cos \theta, r_0 \, r \, \sin \phi \, \cos \theta, - r_0 \, r \, \sin \theta \right) \\
+			\textbf{e}_\phi &= \left( - r_0 \, r \, \sin \phi \, \sin \theta, r_0 \, r\, \cos \phi \, \sin \theta, 0 \right)
 		\end{aligned}
     @f}
 */
 dIR3 morphism_spherical::del(const IR3 &q) const {
 	double r = q[IR3::u];
-	double cn_theta = std::cos(q[IR3::w]);
-	double sn_theta = std::sin(q[IR3::w]);
-	double cn_phi = std::cos(q[IR3::v]);
-	double sn_phi = std::sin(q[IR3::v]);
+	double cn_theta = r0_ * std::cos(q[IR3::v]);
+	double sn_theta = r0_ * std::sin(q[IR3::v]);
+	double cn_phi = std::cos(q[IR3::w]);
+	double sn_phi = std::sin(q[IR3::w]);
 	return {
-		cn_phi * sn_theta, r * cn_phi * cn_theta, - r * sn_phi * sn_theta,
-		sn_phi * sn_theta, r * sn_phi * cn_theta,   r * cn_phi * sn_theta,
-				 cn_theta, 		  - r * sn_theta, 	0
+		cn_phi * sn_theta,  r * cn_phi * cn_theta, -r * sn_phi * sn_theta,
+		sn_phi * sn_theta,  r * sn_phi * cn_theta,  r * cn_phi * sn_theta,
+				 cn_theta, -r * 		 sn_theta,  		0
 	};
 }
 
 //! Returns the morphism's second derivatives, calculated in point @f$ q^\alpha @f$.
-ddIR3 morphism_spherical::del2(const IR3 &q) const {
+ddIR3 morphism_spherical::ddel(const IR3 &q) const {
 	double r = q[IR3::u];
-	double cn_theta = std::cos(q[IR3::w]);
-	double sn_theta = std::sin(q[IR3::w]);
-	double cn_phi = std::cos(q[IR3::v]);
-	double sn_phi = std::sin(q[IR3::v]);
+	double cn_theta = r0_ * std::cos(q[IR3::v]);
+	double sn_theta = r0_ * std::sin(q[IR3::v]);
+	double cn_phi = std::cos(q[IR3::w]);
+	double sn_phi = std::sin(q[IR3::w]);
 	return {
-	// iuu		iuv					iuw						ivv						ivw						iww
+	// iuu		iuv						iuw					ivv						ivw						iww
 		0, cn_phi * cn_theta, -sn_phi * sn_theta, -r * cn_phi * sn_theta, -r * sn_phi * cn_theta, -r * cn_phi * sn_theta,
 		0, sn_phi * cn_theta,  cn_phi * sn_theta, -r * sn_phi * sn_theta,  r * cn_phi * cn_theta, -r * sn_phi * sn_theta,
-		0, 		   -sn_theta,				   0, 		   -r * cn_theta,					   0,		0
+		0, 		   -sn_theta,				   0, 		   -r * cn_theta,					   0,			0
 	};
 }
 
 //! General-purpose implementation of the Jacobian of the transformation in point @f$ q^\alpha @f$.
 /*!
 	Implements the Jacobian in spherical coordinates: 
-	@f$ J = r^2 \, \sin \theta @f$
+	@f$ J = r_0^3 \, r^2 \, \sin \theta @f$
 */
 double morphism_spherical::jacobian(const IR3 &q) const {
-	return q[IR3::u] * q[IR3::u] * std::sin(q[IR3::v]);
+	return r0_3_ * q[IR3::u] * q[IR3::u] * std::sin(q[IR3::v]);
 }
 
 //! Returns the morphism's inverse derivatives, correspondent to the contravariant basis vectors in point @f$ q^\alpha @f$.
@@ -111,23 +116,24 @@ double morphism_spherical::jacobian(const IR3 &q) const {
 	Implements the inverse transformation's first derivatives:
 	@f{gather*}{
 		\begin{aligned}
-			\textbf{e}^r &= \left( \cos \phi \, \sin \theta, \sin \phi \, \sin \theta, \cos \theta \right) \\
-			\textbf{e}^\theta &= \left( \frac{1}{r} \, \cos \phi \, \cos \theta, \frac{1}{r} \, \sin \phi \, \cos \theta, -\frac{1}{r} \, \sin \theta \right) \\
-			\textbf{e}^\phi &= \left( -\frac{\sin \phi}{r \, \sin \theta}, \frac{\cos \phi}{r \, \sin \theta}, 0 \right)
+			\textbf{e}^r &= \left( \frac{1}{r_0} \, \cos \phi \, \sin \theta, \frac{1}{r_0} \, \sin \phi \, \sin \theta, \frac{1}{r_0} \, \cos \theta \right) \\
+			\textbf{e}^\theta &= \left( \frac{1}{r_0 \, r} \, \cos \phi \, \cos \theta, \frac{1}{r_0 \, r} \, \sin \phi \, \cos \theta, -\frac{1}{r_0 \, r} \, \sin \theta \right) \\
+			\textbf{e}^\phi &= \left( -\frac{\sin \phi}{r_0 \, r \, \sin \theta}, \frac{\cos \phi}{r_0 \, r \, \sin \theta}, 0 \right)
 		\end{aligned}
     @f}
 */
 dIR3 morphism_spherical::del_inverse(const IR3 &q) const {
 	double ir = 1 / q[IR3::u];
-	double cn_theta = std::cos(q[IR3::w]);
-	double sn_theta = std::sin(q[IR3::w]);
-	double csc_theta = 1 / sn_theta;
-	double cn_phi = std::cos(q[IR3::v]);
-	double sn_phi = std::sin(q[IR3::v]);
+	double cn_theta = ir0_ * std::cos(q[IR3::v]);
+	double sn_theta = std::sin(q[IR3::v]);
+	double csc_theta = ir0_ / sn_theta;
+	sn_theta *= ir0_;
+	double cn_phi = std::cos(q[IR3::w]);
+	double sn_phi = std::sin(q[IR3::w]);
 	return {
-			   cn_phi * sn_theta, 		sn_phi * sn_theta, 		  cn_theta,
-		  ir * cn_phi * cn_theta,  ir * sn_phi * cn_theta, - ir * sn_theta,
-		- ir * sn_phi * csc_theta, ir * cn_phi * csc_theta, 	0
+			   cn_phi * sn_theta, 		 sn_phi * sn_theta, 	   cn_theta,
+		  ir * cn_phi * cn_theta,   ir * sn_phi * cn_theta, - ir * sn_theta,
+		- ir * sn_phi * csc_theta,  ir * cn_phi * csc_theta, 	0
 	};
 }
 
