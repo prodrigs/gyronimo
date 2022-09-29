c7b57dce6ac6859231fd93feb200a78e7716dd90
tikoh-station <mannyassuncao@hotmail.com>
2022-09-29 23:42:09 +0100

    Add reference factor 'L_0' to metric_cylindrical

---
+++ gyronimo/metrics/metric_cylindrical.cc
@@ -0,0 +1,54 @@
+// ::gyronimo:: - gyromotion for the people, by the people -
+// An object-oriented library for gyromotion applications in plasma physics.
+// Copyright (C) 2022 Manuel Assunção.
+
+// ::gyronimo:: is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// ::gyronimo:: is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with ::gyronimo::.  If not, see <https://www.gnu.org/licenses/>.
+
+// @metric_cylindrical.cc, this file is part of ::gyronimo::
+
+#include <cmath>
+#include <gyronimo/metrics/metric_cylindrical.hh>
+
+namespace gyronimo {
+
+SM3 metric_cylindrical::operator()(const IR3& q) const {
+	return {L0_2_, 0.0, 0.0, L0_2_*q[IR3::u]*q[IR3::u], 0.0, L0_2_};
+}
+
+SM3 metric_cylindrical::inverse(const IR3& q) const {
+	return {iL0_2_, 0.0, 0.0, iL0_2_/(q[IR3::u]*q[IR3::u]), 0.0, iL0_2_};
+}
+
+dSM3 metric_cylindrical::del(const IR3& q) const {
+	return {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 2*q[IR3::u]*L0_2_, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+}
+
+double metric_cylindrical::jacobian(const IR3& q) const {
+	return L0_3_*q[IR3::u];
+}
+
+IR3 metric_cylindrical::del_jacobian(const IR3& q) const {
+	return {L0_2_*std::cos(q[IR3::v]), L0_2_*std::sin(q[IR3::v]), 0.0};
+}
+
+IR3 metric_cylindrical::to_covariant(const IR3& B, const IR3& q) const {
+	return {L0_2_*B[IR3::u], L0_2_*B[IR3::v]*q[IR3::u]*q[IR3::u], L0_2_*B[IR3::w]};
+}
+
+IR3 metric_cylindrical::to_contravariant(const IR3& B, const IR3& q) const {
+	return {iL0_2_*B[IR3::u], iL0_2_*B[IR3::v]/(q[IR3::u]*q[IR3::u]), iL0_2_*B[IR3::w]};
+}
+
+} // end namespace gyronimo
--- gyronimo/metrics/metric_cylindrical.hh
+++ gyronimo/metrics/metric_cylindrical.hh
@@ -1,3 +1,21 @@
+// ::gyronimo:: - gyromotion for the people, by the people -
+// An object-oriented library for gyromotion applications in plasma physics.
+// Copyright (C) 2022 Manuel Assunção.
+
+// ::gyronimo:: is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// ::gyronimo:: is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with ::gyronimo::.  If not, see <https://www.gnu.org/licenses/>.
+
+// @metric_cylindrical.hh, this file is part of ::gyronimo::
 
 #ifndef GYRONIMO_METRIC_CYLINDRICAL
 #define GYRONIMO_METRIC_CYLINDRICAL
@@ -8,29 +26,25 @@ namespace gyronimo {
 
 //! Trivial covariant metric for cylindrical space.
 class metric_cylindrical : public metric_covariant {
- public:
-  metric_cylindrical() {};
-  virtual ~metric_cylindrical() override {};
+public:
+	metric_cylindrical(const double &L0) 
+		: L0_(L0), L0_2_(L0*L0), iL0_2_(1/L0_2_), L0_3_(L0*L0*L0){};
+	virtual ~metric_cylindrical() override {};
 
-  virtual SM3 operator()(const IR3& q) const override {
-    return {1.0, 0.0, 0.0, q[IR3::u]*q[IR3::u], 0.0, 1.0};
-  };
-  virtual dSM3 del(const IR3& q) const override {
-    return {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 2*q[IR3::u], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-  };
-  virtual double jacobian(const IR3& q) const override {
-    return q[IR3::u];
-  };
-  virtual IR3 del_jacobian(const IR3& q) const override {
-    return {std::cos(q[IR3::v]), std::sin(q[IR3::v]), 0.0};
-  };
-  virtual IR3 to_covariant(const IR3& B, const IR3& q) const override {
-    return {B[IR3::u], B[IR3::v]*q[IR3::u]*q[IR3::u], B[IR3::w]};
-  };
-  virtual IR3 to_contravariant(const IR3& B, const IR3& q) const override {
-    return {B[IR3::u], B[IR3::v]/(q[IR3::u]*q[IR3::u]), B[IR3::w]};
-  };
+	virtual SM3 operator()(const IR3& q) const override;
+	virtual SM3 inverse(const IR3& q) const override;
+
+	virtual dSM3 del(const IR3& q) const override;
+	virtual double jacobian(const IR3& q) const override;
+	virtual IR3 del_jacobian(const IR3& q) const override;
+	virtual IR3 to_covariant(const IR3& B, const IR3& q) const override;
+	virtual IR3 to_contravariant(const IR3& B, const IR3& q) const override;
+
+	double L0() {return L0_;};
+
+private:
+	const double L0_, L0_2_, iL0_2_, L0_3_;
+
 }; // end class metric_cylindrical
 
 } // end namespace gyronimo.
